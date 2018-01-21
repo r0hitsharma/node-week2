b940481c76c3aeb80dba73199c5fc696e9d404f0
r0hitsharma <rs@rohit.sh>
2018-01-21 13:12:42 +0100

    Init project and import Contact classes.

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const { Contact, ContactList } = require("./ContactList.js");
+
+// const library = require("./ContactList.js");
+// const Contact = library.Contact;
+// const ContactList = library.ContactList;
+
+console.log(Contact);
+console.log(ContactList);
