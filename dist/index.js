220cff2803f9ddf055253aa4145402b5caa944a0
r0hitsharma <rs@rohit.sh>
2018-01-21 15:13:10 +0100

    Simple http webserver example.

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,32 @@ var _http = require("http");
 
 var http = _interopRequireWildcard(_http);
 
-var _ContactList = require("./ContactList.js");
+var _fs = require("fs");
+
+var fs = _interopRequireWildcard(_fs);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-console.log(_ContactList.Contact);
-console.log(_ContactList.ContactList);
+// import { Contact, ContactList } from"./ContactList.js";
+
+// console.log(Contact);
+// console.log(ContactList);
+
+http.createServer(function (req, res) {
+	console.log(req.method, req.url);
+	// res.write("Hello HYF!");
+	// res.write("This is a simple http server.");
+
+	fs.readFile("./public" + req.url, "utf8", function (err, data) {
+		if (err) {
+			console.log("Error reading file:", err);
+			res.statusCode = 404;
+			res.write("File not found.");
+			return res.end();
+		}
+		// console.log("Data:", data);
+		res.write(data);
+		res.end();
+	});
+}).listen(8080);
 //# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import * as http from "http";
-import { Contact, ContactList } from"./ContactList.js";
+// import { Contact, ContactList } from"./ContactList.js";
 
-console.log(Contact);
-console.log(ContactList);
+// console.log(Contact);
+// console.log(ContactList);
+
+import * as http from "http";
+import * as fs from "fs";
+
+http
+.createServer((req, res) => {
+	console.log(req.method, req.url);
+	// res.write("Hello HYF!");
+	// res.write("This is a simple http server.");
+
+	fs.readFile(`./public${ req.url }`, "utf8", (err, data) => {
+		if(err){
+			console.log("Error reading file:", err);
+			res.statusCode = 404;
+			res.write("File not found.");
+			return res.end();
+		}
+		// console.log("Data:", data);
+		res.write(data);
+		res.end();
+	});
+})
+.listen(8080);
